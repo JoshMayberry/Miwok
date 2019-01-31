2faa48875102e26234bfec9268f4c130596dd0e7
HOME\JoshuaMayberry <joshua.mayberry1991@gmail.com>
2019-01-30 19:20:19 -0500

    Progressed the tutorial
    https://classroom.udacity.com/nanodegrees/nd803/parts/f727e50b-703c-4170-bd6a-dca19cc04283/modules/7e414fd4-48ba-4939-852d-43e292a1d32a/lessons/a1937649-3229-4f89-a0c6-3ae84a42813a/concepts/78832374580923
    https://classroom.udacity.com/nanodegrees/nd803/parts/f727e50b-703c-4170-bd6a-dca19cc04283/modules/7e414fd4-48ba-4939-852d-43e292a1d32a/lessons/a1937649-3229-4f89-a0c6-3ae84a42813a/concepts/79237803690923

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,13 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +16,36 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Log.v("NumbersActivity", "The words at index 0 is: " + words.get(0));
+
+        ListView rootView = (ListView) findViewById(R.id.rootView);
+
+        //Use an adapter to decrease memory usage
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, words);
+        rootView.setAdapter(itemsAdapter);
+
+
+
+        /*
+        for (String word: words) {
+            TextView wordView = new TextView(this);
+            wordView.setText(word);
+            rootView.addView(wordView);
+        }
+        */
+
     }
 }
